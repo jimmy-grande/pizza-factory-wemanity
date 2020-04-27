187368f904ec87ef78f33cad2709c24777edcf0a
Jimmy Grande <jimmy.grande@outlook.com>
2020-04-27 11:30:38 +0200

    make small 4 season with mandatory toppings: test ok

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 class PizzaMaker {
     constructor(inputs) {
+
+        checkIfAnyTopping(inputs.toppings)
+
         this.size = inputs.size
         this.type = inputs.type
         this.toppings = inputs.toppings
     }
+}
+
+function checkIfAnyTopping(toppings) {
+    if(toppings.length == 0)
+        throw new Error('You should provide at least 1 topping')
 }
 
 module.exports = {
